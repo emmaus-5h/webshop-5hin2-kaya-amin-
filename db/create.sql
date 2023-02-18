02cc46aa761d7ff7ff0ded5a9cd86c16e40ccfbf
kaya Alin <93324939+kaya821@users.noreply.github.com>
2023-02-18 19:44:58 +0000

    nigga balls

--- db/create.sql
+++ db/create.sql
@@ -43,16 +43,16 @@ CREATE TABLE energiebron (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id) values ('1-series', 'kleine compacte achterwiel aangedreven auto', '1', 13, 1, 3, 1;
-insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id) values ('2-series', 'sportieve achterwiel aangedreven auto', '2', 13, 2, 3, 2;
-insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id) values ('3-series', 'sportieve sedan', '3', 13, 3, 2, 2;
-insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id) values ('4-series', 'sportieve kleine coupe', '4', 13, 4, 2, 2;
-  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id) values ('5-series', 'grote sedan', '5', 13, 5 , 1, 2;
-  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id) values ('6-series', 'grote sportieve coupe', '6', 13, 6, 2, 3 ;
-  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id) values ('7-series', 'grote luxe limousine', '7', 13, 7, 1, 1;
-  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id) values ('8-series', 'supercar coupe', '8', 13, 8, 2, 3;
-  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id) values ('x5-series', 'grote hoge SUV', '9', 13, 9, 1, 1;
-  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id) values ('x6-series', 'grote hoge SUV met coupe look ', '10', 13, 10, 1, 1;
+insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('1-series', 'kleine compacte achterwiel aangedreven auto', '1', 13, 1, 3, 1, 3;
+insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('2-series', 'sportieve achterwiel aangedreven auto', '2', 13, 2, 3, 2, 2;
+insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('3-series', 'sportieve sedan', '3', 13, 3, 2, 2, 1;
+insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('4-series', 'sportieve kleine coupe', '4', 13, 4, 2, 2, 1;
+  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('5-series', 'grote sedan', '5', 13, 5 , 1, 2, 1;
+  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('6-series', 'grote sportieve coupe', '6', 13, 6, 2, 3, 1;
+  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('7-series', 'grote luxe limousine', '7', 13, 7, 1, 1, 1;
+  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('8-series', 'supercar coupe', '8', 13, 8, 2, 3, 1;
+  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('x5-series', 'grote hoge SUV', '9', 13, 9, 1, 1, 1;
+  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('x6-series', 'grote hoge SUV met coupe look ', '10', 13, 10, 1, 1, 1;
   
   
   

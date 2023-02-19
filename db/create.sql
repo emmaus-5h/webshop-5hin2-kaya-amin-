aa6676d2cad2d2316804de925d61963aeea42ded
kaya Alin <93324939+kaya821@users.noreply.github.com>
2023-02-19 17:40:04 +0000

    hoofdletters

--- db/create.sql
+++ db/create.sql
@@ -43,16 +43,16 @@ CREATE TABLE energiebronnen (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('1-series', 'kleine compacte achterwiel aangedreven auto', '1', 13, 1, 3, 1, 3);
-insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('2-series', 'sportieve achterwiel aangedreven auto', '2', 13, 2, 3, 2, 2);
-insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('3-series', 'sportieve sedan', '3', 13, 3, 2, 2, 1);
-insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('4-series', 'sportieve kleine coupe', '4', 13, 4, 2, 2, 1);
-  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('5-series', 'grote sedan', '5', 13, 5 , 1, 2, 1);
-  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('6-series', 'grote sportieve coupe', '6', 13, 6, 2, 3, 1);
-  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('7-series', 'grote luxe limousine', '7', 13, 7, 1, 1, 1);
-  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('8-series', 'supercar coupe', '8', 13, 8, 2, 3, 1);
-  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('x5-series', 'grote hoge SUV', '9', 13, 9, 1, 1, 1);
-  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('x6-series', 'grote hoge SUV met coupe look ', '10', 13, 10, 1, 1, 1);
+insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('1-Series', 'Kleine compacte achterwiel aangedreven auto', '1', 13, 1, 3, 1, 3);
+insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('2-Series', 'Sportieve achterwiel aangedreven auto', '2', 13, 2, 3, 2, 2);
+insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('3-Series', 'Sportieve sedan', '3', 13, 3, 2, 2, 1);
+insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('4-Series', 'Sportieve kleine coupe', '4', 13, 4, 2, 2, 1);
+  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('5-Series', 'Grote sedan', '5', 13, 5 , 1, 2, 1);
+  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('6-Series', 'Grote sportieve coupe', '6', 13, 6, 2, 3, 1);
+  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('7-Series', 'Grote luxe limousine', '7', 13, 7, 1, 1, 1);
+  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('8-Series', 'Supercar coupe', '8', 13, 8, 2, 3, 1);
+  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('X5-Series', 'Grote hoge SUV', '9', 13, 9, 1, 1, 1);
+  insert into products ( name, discription, code, price, model_id, rating_id, topsnelheid_id, energiebron_id) values ('X6-Series', 'Grote hoge SUV met coupe look ', '10', 13, 10, 1, 1, 1);
   
   
   
@@ -65,14 +65,14 @@ insert into models (model) values ('5-Series');
 insert into models (model) values ('6-series');
 insert into models (model) values ('7-Series');  
 insert into models (model) values ('8-Series');  
-insert into models (model) values ('x5-series');
-insert into models (model) values ('x6-series');
+insert into models (model) values ('X5-Series');
+insert into models (model) values ('X6-Series');
 
 insert into ratings (rating) values ("heel veilig");
 insert into ratings (rating) values ("standaard veilig");
 insert into ratings (rating) values ("minimaal veilig");
 
-insert into topsnelheden (topsnelheid) values ("lage topsnelheid");
+insert into topsnelheden (topsnelheid) values ("Lage topsnelheid");
 insert into topsnelheden (topsnelheid) values ("Gewone topsnelheid");
 insert into topsnelheden (topsnelheid) values ("Hoge topnelheid");
 
